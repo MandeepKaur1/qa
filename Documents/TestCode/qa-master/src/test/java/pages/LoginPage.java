88097b36b91768bacd24aa716fda1db693f74ca9
MandeepKaur1 <mandeepjanjua47@gmail.com>
2018-03-26 13:24:06 -0700

    Refactored code

---
+++ Documents/TestCode/qa-master/src/test/java/pages/LoginPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by mandeep.
+ */
+public class LoginPage {
+
+    public static final By txtDomainName = By.cssSelector("#domain");
+    public static final By btnContinue = By.cssSelector("div.submit-button > button");
+
+    public static final By txtUserName = By.cssSelector("#username");
+    public static final By txtUserPassword = By.cssSelector("#password");
+    public static final By btnLogin = By.cssSelector("div.submit-button");
+    public static final By lnkForgetPassword = By.cssSelector("div:nth-child(2) > div > a");
+}
